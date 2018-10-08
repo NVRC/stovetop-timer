c1350c02f5af5f46dbbcedb47014c643d3748a42
NVRC <nathaniel.charlebois@gmail.com>
2018-10-08 17:28:18 -0400

    Improved sec->min->hour context rotation and UI updates

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import {    StyleSheet,
 import { Col, Row, Grid } from "react-native-easy-grid";
 import ProgressCircle from 'react-native-progress-circle';
 
+
+
 const frame =   {
                     x: Dimensions.get('window').width,
                     y: Dimensions.get('window').height
@@ -15,15 +17,77 @@ const frame =   {
 
 const xDivider = frame.x / 2;
 const yDivider = frame.y / 2;
+const rad = frame.x / 8;
+const borderW = 8;
+/*
+(0,0)
+    ------ xDiv -------
+    |                 |
+    |                 |
+    |                 |
+    |                 |
+   yDiv               |
+    |                 |
+    |                 |
+    |                 |
+    |                 |
+    -------     -------
+
+*/
+
+const rotationalIncrementationBuffer = 15;
+
+
+const elementArray = [
+    'topleft',
+    'topright',
+    'bottomleft',
+    'bottomright',
+    'oven'
+];
+
 
 const defaultState = {
     elements: [
-        { name: 'topleft', angle: 0, active: false },
-        { name: 'topright', angle: 0, active: false },
-        { name: 'bottomleft', angle: 0, active: false },
-        { name: 'bottomright', angle: 0, active: false }
+        { name: 'topleft', angle: 0, active: false, radius:  rad, border: borderW},
+        { name: 'topright', angle: 0, active: false, radius:  rad, border: borderW },
+        { name: 'bottomleft', angle: 0, active: false, radius:  rad, border: borderW },
+        { name: 'bottomright', angle: 0, active: false, radius:  rad, border: borderW }
     ],
 };
+
+//  Display Vars
+const shdClr = "#999";
+const countdownColor = "#F1A94E";
+const windupColor = "#44B3C2";
+const alertColor = "#E45541";
+const backgroundColor = "#fff";
+
+
+const SECONDS_LABEL = 's';
+const MINUTES_LABEL = 'm';
+const HOURS_LABEL = 'h';
+
+const defaultTime = {
+    sec: 0,
+    min: 0,
+    hours: 0,
+};
+
+const timer = require('react-native-timer');
+
+// timers maintained in the Map timer.intervals
+timer.setInterval('SecondsThread', () =>{
+    //  Decrement elements
+    elementArray.forEach((name)=>{
+        if(timer.intervalExists(name)){
+            console.log('decrement :' + name);
+        }
+    });
+
+}, 1000);
+
+
 
 
 
@@ -37,21 +101,83 @@ export default class App extends React.Component {
 
         this.state = {
             elements: [
-                { name: 'topleft', angle: 0, active: false },
-                { name: 'topright', angle: 0, active: false },
-                { name: 'bottomleft', angle: 0, active: false },
-                { name: 'bottomright', angle: 0, active: false }
+                { name: 'topleft', angle: 0, active: false, radius:  rad, border: borderW, color: windupColor, incState: SECONDS_LABEL, time: defaultTime, },
+                { name: 'topright', angle: 0, active: false, radius:  rad, border: borderW, color: windupColor, incState: SECONDS_LABEL, time: defaultTime, },
+                { name: 'bottomleft', angle: 0, active: false, radius:  rad, border: borderW, color: windupColor, incState: SECONDS_LABEL, time: defaultTime, },
+                { name: 'bottomright', angle: 0, active: false, radius:  rad, border: borderW, color: windupColor, incState: SECONDS_LABEL, time: defaultTime, }
             ],
         };
-        console.log('Frame X:'+xDivider);
-        console.log('Frame Y:'+yDivider);
 
+    }
+
+    selectNextState( state, dirForward ){
+        switch (state) {
+            case SECONDS_LABEL:
+                return {
+                    label: dirForward == true ? MINUTES_LABEL : SECONDS_LABEL,
+                    mods: this.getCSSMods(MINUTES_LABEL),
+                };
+                break;
+
+            case MINUTES_LABEL:
+                return {
+                    label: dirForward == true ? HOURS_LABEL : SECONDS_LABEL,
+                    mods: this.getCSSMods(HOURS_LABEL),
+                };
+                break;
+
+            case HOURS_LABEL:
+                let labelString;
+                if (dirForward == 'decrement'){
+                    labelString = HOURS_LABEL;
+                } else if (dirForward){
+                    labelString = HOURS_LABEL;
+                } else if (!dirForward) {
+                    labelString = MINUTES_LABEL;
+                }
+                return {
+                    label: labelString,
+                    mods: this.getCSSMods(HOURS_LABEL),
+                };
+                break;
+            default:
+                break;
+        }
+    }
+
+    getCSSMods( label ){
+        switch (label) {
+            case SECONDS_LABEL:
+                return {
+                    borderMod: 1,
+                    radiusMod: 10,
+                };
+                break;
+
+            case MINUTES_LABEL:
+                return {
+                    borderMod: 2,
+                    radiusMod: 12,
+                };
+                break;
+
+            case HOURS_LABEL:
+                return {
+                    borderMod: 3,
+                    radiusMod: 14,
+                };
+                break;
+            default:
+                return {
+                    borderMod: 0,
+                    radiusMod: 0,
+                };
+                break;
+        }
     }
 
 
     setActiveElement({ moveX, moveY, dx, dy }) {
-        console.log('moveX: '+moveX);
-        console.log('moveY: '+moveY);
         let tag = '';
 
         //
@@ -69,9 +195,62 @@ export default class App extends React.Component {
         //  Update the current State once a burner has been selected
         let elements = [...this.state.elements];
         let index = elements.findIndex(el => el.name === tag);
+
+        //  Cache previous angle
+
+        let prevAngle = elements[index].angle;
+
+        let computedAngle = this.cartesianToAngle(moveX,moveY,tag);
+        let computedMods;
+        let tempState = elements[index].incState;
+        let tempBorder;
+        let tempRad;
+        let tempTime = elements[index].time;
+
+        let computedState;
+        //  Rad case
+        if (prevAngle > 360 - rotationalIncrementationBuffer &&
+                computedAngle > 0 &&
+                computedAngle < rotationalIncrementationBuffer){
+                    computedState = this.selectNextState(elements[index].incState, true);
+                    if ( computedState.label == HOURS_LABEL){
+                        tempTime.hours = tempTime.hours + 1;
+                    }
+                    tempState = computedState.label;
+                    tempBorder = computedState.mods.borderMod;
+                    tempRad = computedState.mods.radiusMod;
+
+        } else if ( prevAngle < rotationalIncrementationBuffer &&
+                computedAngle > 360 - rotationalIncrementationBuffer){
+                    if (tempTime.hours == 1){
+                        computedState = this.selectNextState(elements[index].incState, false);
+                    } else {
+                        computedState = this.selectNextState(elements[index].incState, 'decrement');
+                    }
+                    if ( computedState.label == HOURS_LABEL){
+                        tempTime.hours = tempTime.hours - 1;
+                    }
+                    tempState = computedState.label;
+                    tempBorder = computedState.mods.borderMod;
+                    tempRad = computedState.mods.radiusMod;
+
+        } else {
+            computedMods = this.getCSSMods(elements[index].incState);
+            tempState = elements[index].incState;
+            tempBorder = computedMods.borderMod;
+            tempRad = computedMods.radiusMod;
+        }
+
+
         elements[index] = {...elements[index],
-            angle: this.cartesianToAngle(moveX,moveY,tag), active: true};
+            angle: computedAngle, active: true,
+            radius: rad + tempRad, border: borderW + tempBorder,
+            incState: tempState, time: tempTime,
+        };
+
         this.setState({ elements });
+
+
     }
 
     cartesianToAngle( x, y , tag){
@@ -106,27 +285,75 @@ export default class App extends React.Component {
         return refAngle < 0 ? ((180 + refAngle) + 180) : refAngle;
     }
 
+
+
     polarToPercentage( degree ){
         return ((degree * 100)/360);
     }
 
-    componentWillMount() {
+    degreeToTime( degree, label, currHour ){
+        let time;
+        let string = '';
+        switch (label) {
+            case SECONDS_LABEL:
+                time = (degree * 60)/360;
+                string = Math.round(time) + SECONDS_LABEL;
+                break;
+            case MINUTES_LABEL:
+                time = (degree * 60)/360;
+                string = Math.round(time) + MINUTES_LABEL;
+                break;
+            case HOURS_LABEL:
+                time = (degree * 60)/360;
+                string = currHour + HOURS_LABEL
+                    + Math.round(time) + MINUTES_LABEL;
+                break;
 
+            default:
+                string = 0 + SECONDS_LABEL;
+                break;
+
+        }
+        return string;
+    }
+
+    timeToPercent( time ){
+
+    }
+
+    calcTime( time ){
+        var totalTime = time.secs + (time.mins * 60) + (time.hours * 60 * 60);
+        console.log('tTotal: '+ totalTime);
+
+        //this.setState({ elements });
+    }
+
+
+
+
+
+    componentWillMount() {
+        // Load the full build.
         this._panResponder = PanResponder.create({
             onStartShouldSetPanResponder: (evt, gestureState) => true,
             onMoveShouldSetPanResponder: (evt, gestureState) => true,
             onPanResponderRelease: (evt, gestureState) => {
                 // The user has released all touches while this view is the
                 // responder. This typically means a gesture has succeeded
-                console.log('Release @ X,Y: '+ gestureState.moveX+ ','+gestureState.moveY );
-
+                //  Update the current State once a burner has been selected
+                let elements = [...this.state.elements];
+                let index = elements.findIndex(el => el.active === true);
+                elements[index] = {...elements[index],
+                    active: false,
+                    radius: rad, border: borderW, color: countdownColor};
+                this.setState({ elements });
                 //  Start Timer logic it to the appropriate element index
                 //  elements.findIndex(el => el.active === true)
 
+
+
             },
-            onPanResponderMove: (evt,gestureState) => {
-                this.setActiveElement(gestureState);
-            }
+            onPanResponderMove: (evt,gestureState) => this.setActiveElement(gestureState),
         });
     }
 
@@ -137,49 +364,57 @@ export default class App extends React.Component {
                 >
                     <ProgressCircle
                                 percent={this.polarToPercentage(this.state.elements[0].angle)}
-                                radius={50}
-                                borderWidth={8}
-                                color="#3399FF"
-                                shadowColor="#999"
-                                bgColor="#fff"
+                                radius={this.state.elements[0].radius}
+                                borderWidth={this.state.elements[0].border}
+                                color={this.state.elements[0].color}
+                                shadowColor={this.shadowColor}
+                                bgColor={this.backgroundColor}
                             >
-                                <Text style={{ fontSize: 18 }}>{this.state.elements[0].angle}</Text>
+                                <Text style={{ fontSize: 18 }}>{this.degreeToTime(this.state.elements[0].angle,
+                                this.state.elements[0].incState,
+                                this.state.elements[0].time.hours)}</Text>
                     </ProgressCircle>
                 </View>
                 <View style={styles.elementWrapper}>
                     <ProgressCircle
                                 percent={this.polarToPercentage(this.state.elements[1].angle)}
-                                radius={50}
-                                borderWidth={8}
-                                color="#3399FF"
-                                shadowColor="#999"
-                                bgColor="#fff"
+                                radius={this.state.elements[1].radius}
+                                borderWidth={this.state.elements[1].border}
+                                color={this.state.elements[1].color}
+                                shadowColor={this.shadowColor}
+                                bgColor={this.backgroundColor}
                             >
-                                <Text style={{ fontSize: 18 }}>{this.state.elements[1].angle}</Text>
+                                <Text style={{ fontSize: 18 }}>{this.degreeToTime(this.state.elements[1].angle,
+                                this.state.elements[1].incState,
+                                this.state.elements[1].time.hours)}</Text>
                     </ProgressCircle>
                 </View>
                 <View style={styles.elementWrapper}>
                     <ProgressCircle
                                 percent={this.polarToPercentage(this.state.elements[2].angle)}
-                                radius={50}
-                                borderWidth={8}
-                                color="#3399FF"
-                                shadowColor="#999"
-                                bgColor="#fff"
+                                radius={this.state.elements[2].radius}
+                                borderWidth={this.state.elements[2].border}
+                                color={this.state.elements[2].color}
+                                shadowColor={this.shadowColor}
+                                bgColor={this.backgroundColor}
                             >
-                                <Text style={{ fontSize: 18 }}>{this.state.elements[2].angle}</Text>
+                                <Text style={{ fontSize: 18 }}>{this.degreeToTime(this.state.elements[2].angle,
+                                this.state.elements[2].incState,
+                                this.state.elements[2].time.hours)}</Text>
                     </ProgressCircle>
                 </View>
                 <View style={styles.elementWrapper}>
                     <ProgressCircle
                                 percent={this.polarToPercentage(this.state.elements[3].angle)}
-                                radius={50}
-                                borderWidth={8}
-                                color="#3399FF"
-                                shadowColor="#999"
-                                bgColor="#fff"
+                                radius={this.state.elements[3].radius}
+                                borderWidth={this.state.elements[3].border}
+                                color={this.state.elements[3].color}
+                                shadowColor={this.shadowColor}
+                                bgColor={this.backgroundColor}
                             >
-                                <Text style={{ fontSize: 18 }}>{this.state.elements[3].angle}</Text>
+                                <Text style={{ fontSize: 18 }}>{this.degreeToTime(this.state.elements[3].angle,
+                                this.state.elements[3].incState,
+                                this.state.elements[3].time.hours)}</Text>
                     </ProgressCircle>
                 </View>
 
